ac9e4a6e28ea1b5ebee32cfb7362edf2fef2d761
kwontaehoon <gju04195@gmail.com>
2022-10-11 21:18:32 +0900

    update

--- component/Home/Album.jsx
+++ component/Home/Album.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, Image, FlatList } from 'react-native'
+import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native'
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage"
 import firebaseConfig from '../../firebase'
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -19,11 +19,6 @@ const a = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
-  right:{
-    borderWidth: 1,
-    borderColor: 'black',
-    flex: 3,
-  },
   main:{
     
   },
@@ -36,12 +31,30 @@ const a = StyleSheet.create({
     marginHorizontal: 16,
   },
   title:{
+    alignItems: 'flex-end',
+    padding: 3,
+  },
+  right:{
+    borderWidth: 2,
+    borderColor: 'black',
+    flex: 3,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  photos:{
     borderWidth: 1,
     borderColor: 'black',
+    width: '33.3333%',
+    height: 60,
   }
+
+
 })
 
 const Album = () => {
+
+
+
 
 const app = firebaseConfig;
 const storage = getStorage();
@@ -52,36 +65,9 @@ const [address, setAddress] = useState([]);
 console.log('address: ', address);
 const [photos, setPhotos] = useState([]);
 console.log('photos: ', photos);
+console.log(photos.length);
+const [album, setAlbum] = useState(0);
 
-const test = [
-  {
-    id: 1,
-    title: 'First Item',
-  },
-  {
-    id: 2,
-    title: 'Second Item',
-  },
-  {
-    id: 3,
-    title: 'Third Item',
-  },
-  {
-    id: 4,
-    title: 'Third Item',
-  },
-  {
-    id: 5,
-    title: 'Third Item',
-  },
-  {
-    id: 6,
-    
-  },
-  {
-    id: 7,
-  }
-];
 
 useEffect(()=>{
   console.log('useEffect');
@@ -92,19 +78,19 @@ const listRef = ref(storage, x);
 
 const func = async() => {
   console.log('func');
-  await listAll(listRef)
+  listAll(listRef)
   .then((res) => {
     let arr = [];
     res.items.forEach((x) => {
       arr.push(x.fullPath);
-    });
-    setAddress(arr);
+    });  
     func2(arr);
   });
 }
 
 const func2 = async(a) => {
   console.log('func2');
+  console.log('a: ', a);
   let arr = [];
   let arr2 = [];
   await a.map(x => {
@@ -112,8 +98,10 @@ const func2 = async(a) => {
     getDownloadURL(starsRef)
     .then((url) => {
       arr.push({url});
+      console.log('arr: ',arr);
       if(arr.length === a.length){
-        arr2 = { id: index, url: arr };
+        console.log('이잉');
+        arr2 = { id: index+1, url: arr };
         kwon.push(arr2);
         setPhotos(kwon);
       }
@@ -124,14 +112,33 @@ func();
 });
 }, []);
 
+const List1 = () => {
+  let arr = [];
+  if(title.length === photos.length){
+    console.log('aaaaaaaaaaaaaaaaaaaaaa');
+    console.log(photos.length);
+    console.log(title.length);
+  photos[album].url.map((x, index) => {
+  switch(true){
+    case album === 2 : arr.push(<Image style={a.photos} source={{uri: x.url}} key={index}></Image>); break;
+    default: arr.push(
+      <Image style={a.photos} source={{uri: x.url}} key={index}></Image>
+      
+    );
+  }
+})
+  }
+return arr;
+}
+
 const renderItem = ({ item }) => (
-  <View>
-    <Image style={a.image} ></Image>
-    <View style={a.title}><Text>gg</Text></View>
-  </View>
+  <TouchableOpacity onPress={() => setAlbum(item.id)}>
+    <Image style={a.image} source={{uri: item.url[0].url}}></Image>
+    <View style={a.title}><Text>앨범{item.id}</Text></View>
+  </TouchableOpacity>
 );
 
-  return photos.length !== 0 ? (
+  return photos.length === 2 ? ( 
     <View style={a.container}>
       <View style={a.left}>
         <View style={a.main}>
@@ -139,7 +146,9 @@ const renderItem = ({ item }) => (
             horizontal={false} />
         </View>
       </View>
-      <View style={a.right}></View>
+      <View style={a.right}>
+        {/* <List1 /> */}
+      </View>
       {/* <Image source={{uri: photos[0]}} style={{width: '50%', height: '50%'}}></Image>
       <Image source={{uri: photos[1]}} style={{width: '50%', height: '50%'}}></Image> */}
       
--- component/Home/test.js
+++ component/Home/test.js
@@ -1,3 +1,7 @@
 const arr = [{id: 1}];
 
 console.log(Object.keys(arr).length);
+
+const arr2 = 1;
+
+arr2 === 1 ? console.log('a') : console.log('b');

6203ab6caf406a33bf3e0bb6c67f8f96353031bb
kwontaehoon <gju04195@gmail.com>
2022-10-15 20:43:24 +0900

    update

--- component/Home/Store.jsx
+++ component/Home/Store.jsx
@@ -1,15 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { useSelector } from 'react-redux'
+import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown';
+
 
 const Store = () => {
 
+  const a = StyleSheet.create({
+    container:{
+      
+    },
+    header:{
+      borderWidth: 1,
+      borderColor: 'black',
+      height: '10%',
+    },
+    main:{
+      borderWidth: 2,
+      borderColor: 'black',
+      height: 500,
+    },
+    box:{
+      backgroundColor: 'pink',
+      height: 200,
+    }
+  })
+
   const number = useSelector(state=>state.info);
-  console.log(number);
+  const [selectedItem, setSelectedItem] = useState(null);
+  console.log('selectItem: ', selectedItem)
+  const [info, setInfo] = useState([]);
+
+  useEffect(()=>{
+    let arr = [];
+    number.filter((x, index)=>{
+      arr.push({id: index+1, title: x.course_name, address: x.address01, tel: x.tel});
+    })
+    setInfo(arr);
+  }, []);
 
   return (
-    <View>
-      <Text>gg</Text>
+    <View style={a.container}>
+      <View style={a.header}><Text>매장명으로 검색</Text></View>
+      <AutocompleteDropdown clearOnFocus={false} closeOnBlur={false}
+      closeOnSubmit={false} initialValue={{ id: 1 }} // or just '2'
+      onSelectItem={setSelectedItem}
+      dataSet={info} />
+
+      <View style={[a.main, {display: selectedItem}]}>
+        <View style={a.box}>
+          <Text>{selectedItem.title}</Text>
+          <Text>{selectedItem.address}</Text>
+          <Text>{selectedItem.tel}</Text>
+        </View>
+      </View>
     </View>
   )
 }
--- component/Home/test.js
+++ component/Home/test.js
@@ -1,7 +1,14 @@
-const arr = [{id: 1}];
+// const arr = [{id: 1}];
 
-console.log(Object.keys(arr).length);
+// console.log(Object.keys(arr).length);
 
-const arr2 = 1;
+// const arr2 = 1;
 
-arr2 === 1 ? console.log('a') : console.log('b');
+// arr2 === 1 ? console.log('a') : console.log('b');
+
+const source = 'execute';
+
+const test = new Set(source);
+console.log(test);
+const test2 = Array.from(test);
+console.log(test2);

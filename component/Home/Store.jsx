861b5ff7fcfe1e44609392c3f96b76fb0f8a5f19
kwontaehoon <gju04195@gmail.com>
2022-10-17 19:15:14 +0900

    update

--- component/Home/Store.jsx
+++ component/Home/Store.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native'
 import { useSelector } from 'react-redux'
 import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown';
 
@@ -10,26 +10,41 @@ const Store = () => {
     container:{
       
     },
-    header:{
-      borderWidth: 1,
-      borderColor: 'black',
-      height: '10%',
+    filter:{
+      width: '100%',
+      height: 100,
+      position: 'absolute',
+      zIndex: 100,
+      bottom: 50,
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      flexDirection: 'row',
+    },
+    button:{
+      width: 150,
+      height: 50,
+      backgroundColor: 'green',
+      borderRadius: 20,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
     main:{
       borderWidth: 2,
       borderColor: 'black',
-      height: 500,
+      height: '90%',
+      backgroundColor: 'pink',
     },
     box:{
-      backgroundColor: 'pink',
-      height: 200,
+      backgroundColor: 'green',
+      height: 150,
     }
   })
 
   const number = useSelector(state=>state.info);
-  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedItem, setSelectedItem] = useState(null); // 매장 선택시
   console.log('selectItem: ', selectedItem)
   const [info, setInfo] = useState([]);
+  console.log('info: ', info);
 
   useEffect(()=>{
     let arr = [];
@@ -39,20 +54,29 @@ const Store = () => {
     setInfo(arr);
   }, []);
 
+  const renderItem = ({ item }) => (
+    <TouchableOpacity>
+    </TouchableOpacity>
+  );
+
   return (
     <View style={a.container}>
-      <View style={a.header}><Text>매장명으로 검색</Text></View>
+      <View style={a.filter}>
+        <TouchableOpacity style={a.button}><Text>매장으로 검색하기</Text></TouchableOpacity>
+        <TouchableOpacity style={a.button}><Text>지역으로 검색하기</Text></TouchableOpacity>
+        
+      </View>
       <AutocompleteDropdown clearOnFocus={false} closeOnBlur={false}
       closeOnSubmit={false} initialValue={{ id: 1 }} // or just '2'
       onSelectItem={setSelectedItem}
       dataSet={info} />
 
-      <View style={[a.main, {display: selectedItem}]}>
-        <View style={a.box}>
-          <Text>{selectedItem.title}</Text>
-          <Text>{selectedItem.address}</Text>
-          <Text>{selectedItem.tel}</Text>
-        </View>
+      <View style={a.main}>
+        <FlatList data={info} renderItem={renderItem}>
+          <View style={a.box}>
+          
+          </View>
+        </FlatList>
       </View>
     </View>
   )

738d22c0051e7744655823690f4b7974bebde0d7
kwontaehoon <gju04195@gmail.com>
2022-10-17 22:56:07 +0900

    update

--- component/Home/Store.jsx
+++ component/Home/Store.jsx
@@ -23,7 +23,7 @@ const Store = () => {
     button:{
       width: 150,
       height: 50,
-      backgroundColor: 'green',
+      backgroundColor: 'white',
       borderRadius: 20,
       justifyContent: 'center',
       alignItems: 'center',
@@ -31,20 +31,33 @@ const Store = () => {
     main:{
       borderWidth: 2,
       borderColor: 'black',
-      height: '90%',
-      backgroundColor: 'pink',
+      height: '77%',
+      marginTop: 100,
+      borderTopEndRadius: 30,
+      borderTopStartRadius: 30,
+      overflow: 'hidden',
+      padding: 10,
     },
     box:{
-      backgroundColor: 'green',
-      height: 150,
+      height: 120,
+      borderBottomWidth: 1,
+      borderColor: 'black',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+    },
+    subbox:{
+      width: '33.4%',
+      alignItems: 'center',
+      justifyContent: 'center',
     }
   })
 
   const number = useSelector(state=>state.info);
-  const [selectedItem, setSelectedItem] = useState(null); // 매장 선택시
+  const [selectedItem, setSelectedItem] = useState(); // 매장 선택시
   console.log('selectItem: ', selectedItem)
   const [info, setInfo] = useState([]);
   console.log('info: ', info);
+  const [filter, setFilter] = useState(false); // 매장 / 지역 버튼 누르면 배경색깔 변경
 
   useEffect(()=>{
     let arr = [];
@@ -52,31 +65,47 @@ const Store = () => {
       arr.push({id: index+1, title: x.course_name, address: x.address01, tel: x.tel});
     })
     setInfo(arr);
+    setSelectedItem(arr);
   }, []);
 
+  const select = (e) => {
+    let arr = [e];
+    setSelectedItem(arr);
+  }
+  const select_text = (e) => {
+    if(e.length === 0){
+      setSelectedItem(info);
+    }else{
+      selectedItem.map((x, index)=>{
+        console.log('x: ', x);
+      })
+    }
+  }
+
   const renderItem = ({ item }) => (
-    <TouchableOpacity>
+    <TouchableOpacity style={a.box}>
+      <View style={a.subbox}><Text>{item.title}</Text></View>
+      <View style={a.subbox}><Text>{item.address}</Text></View>
+      <View style={a.subbox}><Text>{item.tel}</Text></View>
     </TouchableOpacity>
-  );
+  ); 
 
   return (
     <View style={a.container}>
       <View style={a.filter}>
-        <TouchableOpacity style={a.button}><Text>매장으로 검색하기</Text></TouchableOpacity>
-        <TouchableOpacity style={a.button}><Text>지역으로 검색하기</Text></TouchableOpacity>
+        <TouchableOpacity style={[a.button, {backgroundColor: filter ? 'white' : 'skyblue'}]} onPress={()=>setFilter(false)}><Text style={{fontSize: 15, fontWeight: 'bold'}}>매장으로 검색하기</Text></TouchableOpacity>
+        <TouchableOpacity style={[a.button, {backgroundColor: filter ? 'skyblue' : 'white'}]} onPress={()=>setFilter(true)}><Text style={{fontSize: 15, fontWeight: 'bold'}}>지역으로 검색하기</Text></TouchableOpacity>
         
       </View>
       <AutocompleteDropdown clearOnFocus={false} closeOnBlur={false}
       closeOnSubmit={false} initialValue={{ id: 1 }} // or just '2'
-      onSelectItem={setSelectedItem}
-      dataSet={info} />
+      onSelectItem={(e)=>select(e)}
+      dataSet={selectedItem} onChangeText={(e)=>select_text(e)}
+      suggestionsListMaxHeight={300}
+      showClear={false}/>
 
       <View style={a.main}>
-        <FlatList data={info} renderItem={renderItem}>
-          <View style={a.box}>
-          
-          </View>
-        </FlatList>
+        <FlatList data={selectedItem} renderItem={renderItem}/>
       </View>
     </View>
   )
